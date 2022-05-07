d985f59259a35ebe9635f83d8d2184088ba04323
Mitchell Woodin <mitchbcrich@hotmail.com>
2022-05-08 09:04:14 +1000

    Adding custom elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet
+} from 'react-router-dom';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import NavBar from './components/NavBar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F66B0E", // Orange
+      contrastText: "#fff"
+    },
+    secondary: {
+      main: "#205375", // Light Blue
+      dark: "#112B3C", // Dark Blue
+      contrastText: "#FFF"
+    },
+    background: {
+      default: "#282c34" // Dark Blue
+    }
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <ThemeProvider theme={theme} sx={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
+      <NavBar />
+      <Container>
         <h1>Mitchell Woodin</h1>
         <p>Portfolio Site</p>
-      </header>
-    </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
